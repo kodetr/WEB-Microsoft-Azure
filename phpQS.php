0ed9b07a20a3b5a7af8413bb7cccc2b4dd6c8d50
codetr <manusia.tados@gmail.com>
2019-03-21 11:28:23 +0800

    update

--- phpQS.php
+++ phpQS.php
@@ -13,9 +13,9 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUN
 
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
+$fileToUpload = "flutter.jpg";
 
 if (!isset($_GET["Cleanup"])) {
-    $fileToUpload = "flutter.jpg";
     // Create container options object.
     $createContainerOptions = new CreateContainerOptions();
 
@@ -99,25 +99,65 @@ if (!isset($_GET["Cleanup"])) {
     //     echo $code.": ".$error_message."<br />";
     // }
 } 
-else 
-{
+// else 
+// {
 
-    try{
-        // Delete container.
-        echo "Deleting Container".PHP_EOL;
-        echo $_GET["containerName"].PHP_EOL;
-        echo "<br />";
-        $blobClient->deleteContainer($_GET["containerName"]);
-    }
-    catch(ServiceException $e){
+//     try{
+//         // Delete container.
+//         echo "Deleting Container".PHP_EOL;
+//         echo $_GET["containerName"].PHP_EOL;
+//         echo "<br />";
+//         $blobClient->deleteContainer($_GET["containerName"]);
+//     }
+//     catch(ServiceException $e){
         // Handle exception based on error codes and messages.
         // Error codes and messages are here:
         // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-}
+    //     $code = $e->getCode();
+    //     $error_message = $e->getMessage();
+    //     echo $code.": ".$error_message."<br />";
+    // }try {
+        //     // Create container.
+        //     $blobClient->createContainer($containerName, $createContainerOptions);
+    
+        //     // Getting local file so that we can upload it to Azure
+        //     $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+        //     fclose($myfile);
+            
+        //     # Upload file as a block blob
+        //     echo "Uploading BlockBlob: ".PHP_EOL;
+        //     echo $fileToUpload;
+        //     echo "<br />";
+            
+        //     $content = fopen($fileToUpload, "r");
+    
+        //     //Upload blob
+        //     $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+    
+        //     // List blobs.
+        //     $listBlobsOptions = new ListBlobsOptions();
+        //     $listBlobsOptions->setPrefix("flutter");
+    
+        //     echo "These are the blobs present in the container: ";
+    
+        //     do{
+        //         $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+        //         foreach ($result->getBlobs() as $blob)
+        //         {
+        //             echo $blob->getName().": ".$blob->getUrl()."<br />";
+        //         }
+            
+        //         $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+        //     } while($result->getContinuationToken());
+        //     echo "<br />";
+    
+        //     // Get blob.
+        //     echo "This is the content of the blob uploaded: ";
+        //     $blob = $blobClient->getBlob($containerName, $fileToUpload);
+        //     fpassthru($blob->getContentStream());
+        //     echo "<br />";
+        // }
+// }
 ?>
 
 
