c6d02dc19cd6f29b423a5428694662711b50757b
codetr <manusia.tados@gmail.com>
2019-03-21 11:25:41 +0800

    update

--- phpQS.php
+++ phpQS.php
@@ -41,47 +41,47 @@ if (!isset($_GET["Cleanup"])) {
 
       $containerName = "blockblobs".generateRandomString();
 
-    try {
-        // Create container.
-        $blobClient->createContainer($containerName, $createContainerOptions);
+    // try {
+    //     // Create container.
+    //     $blobClient->createContainer($containerName, $createContainerOptions);
 
-        // Getting local file so that we can upload it to Azure
-        $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
-        fclose($myfile);
+    //     // Getting local file so that we can upload it to Azure
+    //     $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+    //     fclose($myfile);
         
-        # Upload file as a block blob
-        echo "Uploading BlockBlob: ".PHP_EOL;
-        echo $fileToUpload;
-        echo "<br />";
+    //     # Upload file as a block blob
+    //     echo "Uploading BlockBlob: ".PHP_EOL;
+    //     echo $fileToUpload;
+    //     echo "<br />";
         
-        $content = fopen($fileToUpload, "r");
+    //     $content = fopen($fileToUpload, "r");
 
-        //Upload blob
-        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+    //     //Upload blob
+    //     $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 
-        // List blobs.
-        $listBlobsOptions = new ListBlobsOptions();
-        $listBlobsOptions->setPrefix("flutter");
+    //     // List blobs.
+    //     $listBlobsOptions = new ListBlobsOptions();
+    //     $listBlobsOptions->setPrefix("flutter");
 
-        echo "These are the blobs present in the container: ";
+    //     echo "These are the blobs present in the container: ";
 
-        do{
-            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-            foreach ($result->getBlobs() as $blob)
-            {
-                echo $blob->getName().": ".$blob->getUrl()."<br />";
-            }
+    //     do{
+    //         $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+    //         foreach ($result->getBlobs() as $blob)
+    //         {
+    //             echo $blob->getName().": ".$blob->getUrl()."<br />";
+    //         }
         
-            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-        } while($result->getContinuationToken());
-        echo "<br />";
+    //         $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+    //     } while($result->getContinuationToken());
+    //     echo "<br />";
 
-        // Get blob.
-        echo "This is the content of the blob uploaded: ";
-        $blob = $blobClient->getBlob($containerName, $fileToUpload);
-        fpassthru($blob->getContentStream());
-        echo "<br />";
-    }
+    //     // Get blob.
+    //     echo "This is the content of the blob uploaded: ";
+    //     $blob = $blobClient->getBlob($containerName, $fileToUpload);
+    //     fpassthru($blob->getContentStream());
+    //     echo "<br />";
+    // }
     // catch(ServiceException $e){
     //     // Handle exception based on error codes and messages.
     //     // Error codes and messages are here:
@@ -90,14 +90,14 @@ if (!isset($_GET["Cleanup"])) {
     //     $error_message = $e->getMessage();
     //     echo $code.": ".$error_message."<br />";
     // }
-    catch(InvalidArgumentTypeException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
+    // catch(InvalidArgumentTypeException $e){
+    //     // Handle exception based on error codes and messages.
+    //     // Error codes and messages are here:
+    //     // http://msdn.microsoft.com/library/azure/dd179439.aspx
+    //     $code = $e->getCode();
+    //     $error_message = $e->getMessage();
+    //     echo $code.": ".$error_message."<br />";
+    // }
 } 
 else 
 {
