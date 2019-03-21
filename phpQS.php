bdb1613b284d37da7df92128cf646303ca9cf6d1
codetr <manusia.tados@gmail.com>
2019-03-21 11:24:39 +0800

    update

--- phpQS.php
+++ phpQS.php
@@ -82,14 +82,14 @@ if (!isset($_GET["Cleanup"])) {
         fpassthru($blob->getContentStream());
         echo "<br />";
     }
-    catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
+    // catch(ServiceException $e){
+    //     // Handle exception based on error codes and messages.
+    //     // Error codes and messages are here:
+    //     // http://msdn.microsoft.com/library/azure/dd179439.aspx
+    //     $code = $e->getCode();
+    //     $error_message = $e->getMessage();
+    //     echo $code.": ".$error_message."<br />";
+    // }
     catch(InvalidArgumentTypeException $e){
         // Handle exception based on error codes and messages.
         // Error codes and messages are here:
